3d0364c5ff5b74fb0d17583b1676ef13d853b853
Kannan Vijayan <kannan@vijayan.ca>
2015-08-20 11:01:38 -0400

    Refactored binary-utils into own file.

---
+++ flyweb/lib/dns-sd/binary-utils.js
@@ -0,0 +1,45 @@
+"use strict";
+
+function stringToArrayBuffer(string) {
+  let length = (string || '').length;
+  let arrayBuffer = new ArrayBuffer(length);
+  let uint8Array = new Uint8Array(arrayBuffer);
+  for (let i = 0; i < length; i++) {
+    uint8Array[i] = string.charCodeAt(i);
+  }
+
+  return arrayBuffer;
+}
+
+function arrayBufferToString(arrayBuffer) {
+  let results = [];
+  let uint8Array = new Uint8Array(arrayBuffer);
+
+  for (let i = 0, length = uint8Array.length; i < length; i += 200000) {
+    let subArray = uint8Array.subarray(i, i + 200000);
+    results.push(String.fromCharCode.apply(null, subArray));
+  }
+
+  return results.join('');
+}
+
+function blobToArrayBuffer(blob, callback) {
+  let fileReader = new FileReader();
+  fileReader.onload = function() {
+    if (typeof callback === 'function') {
+      callback(fileReader.result);
+    }
+  };
+  fileReader.readAsArrayBuffer(blob);
+
+  return fileReader.result;
+}
+
+function mergeArrayBuffers(arrayBuffers, callback) {
+  return blobToArrayBuffer(new Blob(arrayBuffers), callback);
+}
+
+exports.stringToArrayBuffer = stringToArrayBuffer;
+exports.arrayBufferToString = arrayBufferToString;
+exports.blobToArrayBuffer = blobToArrayBuffer;
+exports.mergeArrayBuffers = mergeArrayBuffers;
--- flyweb/lib/dns-sd/dns-sd.js
+++ flyweb/lib/dns-sd/dns-sd.js
@@ -2,53 +2,10 @@ var { emit, on, once, off } = require("sdk/event/core");
 var {Cc, Ci, Cu} = require("chrome");
 
 var utils = require("../utils");
+var BinaryUtils = require('./binary-utils');
 
 /* The following was modified from https://github.com/justindarc/dns-sd.js */
 
-
-/**
- * BinaryUtils
- */
-
-var BinaryUtils = {
-  stringToArrayBuffer: function(string) {
-    var length = (string || '').length;
-    var arrayBuffer = new ArrayBuffer(length);
-    var uint8Array = new Uint8Array(arrayBuffer);
-    for (var i = 0; i < length; i++) {
-      uint8Array[i] = string.charCodeAt(i);
-    }
-
-    return arrayBuffer;
-  },
-
-  arrayBufferToString: function(arrayBuffer) {
-    var results = [];
-    var uint8Array = new Uint8Array(arrayBuffer);
-
-    for (var i = 0, length = uint8Array.length; i < length; i += 200000) {
-      results.push(String.fromCharCode.apply(null, uint8Array.subarray(i, i + 200000)));
-    }
-
-    return results.join('');
-  },
-
-  blobToArrayBuffer: function(blob, callback) {
-    var fileReader = new FileReader();
-    fileReader.onload = function() {
-      if (typeof callback === 'function') {
-        callback(fileReader.result);
-      }
-    };
-    fileReader.readAsArrayBuffer(blob);
-
-    return fileReader.result;
-  },
-
-  mergeArrayBuffers: function(arrayBuffers, callback) {
-    return BinaryUtils.blobToArrayBuffer(new Blob(arrayBuffers), callback);
-  }
-};
 
 /**
  * ByteArray
